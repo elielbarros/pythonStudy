ff6f161b92c7aeff9815d5dfad12346d26470ac7
Eliel Silva <elielbarros.silva@gmail.com>
2023-10-30 22:49:39 -0300

    include arithmetic operation

---
+++ section3/arithmeticOperation.py
@@ -0,0 +1,27 @@
+# sum
+sum = 1 + 1
+print(sum)
+
+# subtraction
+subtraction = 1 - 1
+print(subtraction)
+
+# multiplication
+multiplication = 1 * 1
+print(multiplication)
+
+# division
+division = 3 / 1.8 # It will always return float
+print(division)
+
+# whole division
+whole_division = 3 // 1.8 # Return float or int and will truncate decimal number
+print(whole_division)
+
+# exponentiation
+exponentiation = 3 ** 3
+print(exponentiation)
+
+# module
+module = 10 % 2 # Will return just the rest of the division
+print(module)
