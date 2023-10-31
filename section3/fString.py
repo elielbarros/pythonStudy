06e7bc16e9dedf1da8e3b65b9c5acabc5c727621
Eliel Silva <elielbarros.silva@gmail.com>
2023-10-30 23:12:18 -0300

    include fString

---
+++ section3/fString.py
@@ -0,0 +1,11 @@
+# Calculate BMI
+name = 'John Doe'
+height = 1.75
+weight = 64
+bmi = weight / height ** 2
+
+first_line = f'{name} is {height} tall'
+second_line = f'Weighs {weight} kg and his bmi is {bmi:.2f}'
+print(first_line)
+print(second_line)
+
