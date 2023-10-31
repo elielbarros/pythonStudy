ac53354895e85e45024cd39c9448b7974c1dcd5c
Eliel Silva <elielbarros.silva@gmail.com>
2023-10-30 22:35:36 -0300

    include exercice0

--- section3/exercise0.py
+++ section3/exercise0.py
@@ -2,3 +2,12 @@ name = "John"
 last_name = "Doe"
 age = 1023
 birthday = "1000/01/01"
+of_age = age > 1023
+height = 1.75
+
+print('Name: ', name)
+print('Last Name: ', last_name)
+print('Age: ', age)
+print('Birthday: ', birthday)
+print('Are you of age? ', of_age)
+print('Height in meters: ', height)
