289e4262ddaee7190a3acb49c8a374d8767fe5c2
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-01 21:48:17 -0300

    include if elif else

---
+++ section3/if_elif_else.py
@@ -0,0 +1,11 @@
+# if / elif / else
+
+input_result = input("Do you want to 'get in' or 'get out'? ")
+
+if input_result == "get in":
+    print("You are in")
+elif input_result == "get out":
+    print("You are out")
+else:
+    print("You must input 'get in' or 'get out'")
+
