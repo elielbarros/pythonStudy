15a7246d84806715c156669db4424959e6e4015a
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-02 16:27:28 -0300

    include variable id

---
+++ section3/variableId.py
@@ -0,0 +1,9 @@
+v1 = 'a'
+v2 = 'a'
+# Python put the variable value into memory.
+# If the value from two different variables are the same...
+# Then Python use the same memory allocated those two variables.
+# We can see by the variable id that is the id used to recognize the element 'a' on memory.
+print(id(v1))
+print(id(v2))
+
