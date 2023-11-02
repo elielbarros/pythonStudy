39d85e38c0f8ed652a756c56e09665777da1fde5
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-02 12:40:56 -0300

    logical operator 'AND'

---
+++ section3/logicalOperatorAnd.py
@@ -0,0 +1,22 @@
+"""
+Logical Operator 'AND'
+
+With 'AND' all operations need to be True to return True
+If one operation come False then the result will be False
+
+With 'OR' if one condition is True then the expression will return True already
+"""
+condition_true = True
+condition_false = False
+
+# 'IF' that will return 'False' because of 'condition_false'
+if condition_true and condition_false:
+    print('Will not print line 12')  # Will not print
+
+if condition_true and condition_true:
+    print('Will print line 17')
+
+# short circuit assessment
+# Will get off from if at the first condition false
+if condition_false and condition_true:
+    print('Will not print line 18')
