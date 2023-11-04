b74252ffaff64d94f18ef7a6086d5c739041b50e
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-04 15:18:46 -0300

    include for with range

---
+++ section3/forAndRange.py
@@ -0,0 +1,11 @@
+"""
+Today I Learned about how get a Range of numbers.
+range(start, stop, step)
+
+Also have used these numbers to use for loop
+"""
+
+numbers = range(0, 100, 10)
+
+for number in numbers:
+    print(number)
--- section3/forLoop.py
+++ section3/forLoop.py
@@ -2,3 +2,7 @@ phrase = 'Hello World!'
 
 for char in phrase:
     print(char, end='')
+
+"""
+Today i learned about for loop and that i can iterate over a String using the auxiliary 'char' as shown above.
+"""
