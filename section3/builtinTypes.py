59bb32702d467daf6b6ef57b8831d91d05b4c413
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-03 22:43:36 -0300

    include builtin Type

---
+++ section3/builtinTypes.py
@@ -0,0 +1,13 @@
+"""
+Immutable Types: str, int, float, bool
+"""
+# string = 'Hello world!'
+# It is not possible to change string value on position 3.
+# If you try to do that, it will raise an error.
+# string[3] = 'Test'  # TypeError: 'str' object does not support item assignment
+
+# print(string)
+
+string = 'Test'
+print(string.zfill(10))  # Will print String with 10 letters, including the string and zero
+
