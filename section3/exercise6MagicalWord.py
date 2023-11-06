d128f89c1670c57cdd8dae89d226884c79cd4dc9
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-06 20:15:27 -0300

    include exercise 6 magical word finished

--- section3/exercise6MagicalWord.py
+++ section3/exercise6MagicalWord.py
@@ -1,7 +1,7 @@
 magical_word = 'hello'
 magical_star = len(magical_word) * '*'
 magical_word_length = len(magical_word)
-discovered_word = ''
+discovered_word = len(magical_word) * '*'
 
 print('')
 print(f'The tip is that the Magical Word has {magical_word_length} letters. {magical_star}')
@@ -15,17 +15,27 @@ while True:
 
     if character.lower() in magical_word.lower():
         index = 0
+        magical_star = ''
         while index < magical_word_length:
-            if character.lower() == magical_word[index]:
-                discovered_word += character.lower()
+            if discovered_word[index] != '*':
+                magical_star += discovered_word[index]
                 index += 1
                 continue
 
-            if len(discovered_word) < magical_word_length:
-                discovered_word += '*'
+            if character.lower() == magical_word[index]:
+                magical_star += character.lower()
+                index += 1
+                continue
+
+            if discovered_word[index] == '*':
+                magical_star += '*'
 
             index += 1
 
-
+        discovered_word = magical_star
     print(discovered_word)
+
+    if discovered_word == magical_word:
+        print("You've discovered the word! Congratulations.")
+        break
 
