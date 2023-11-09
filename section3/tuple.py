3b5626d6b4698901235f44b5de42756f35977ca2
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-08 22:43:15 -0300

    include tuple

---
+++ section3/tuple.py
@@ -0,0 +1,24 @@
+"""
+TUPLE
+
+Tuple does not support item assignment
+
+If I want a list that it is not possible to remove or edit it is possible to use a tuple
+
+Tuple could be used with parentheses or without
+"""
+
+names = "John", "Mary", "David"
+print(names, type(names))
+print(names[0])
+
+# It is possible to create a tuple by list
+list_a = ["John", "Mary", "David"]
+tuple_a = tuple(list_a)
+print(tuple_a, type(tuple_a), list_a, type(list_a))
+
+# It is possible to create a list by tuple
+tuple_b = ("John", "Mary", "David")
+list_b = list(tuple_b)
+print(list_b, type(list_b))
+
