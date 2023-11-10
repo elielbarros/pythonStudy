568960c6a8efc092361a1a58bcfe266ee5551a14
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-09 23:14:16 -0300

    include exercise 9 CPF generator

---
+++ section3/exercise9CPFGenerator.py
@@ -0,0 +1,28 @@
+"""
+Search for: 'Algoritmo de Validação do CPF'
+
+CPF: 746.824.890-70
+"""
+
+cpf = '746.824.890-70'
+split_cpf = cpf.split('-')
+numbers_to_multiply = 10
+sum_ = 0
+for digit in split_cpf[0]:
+    try:
+        digit_int = int(digit)
+        # print(digit_int)
+        result_multiplication = digit_int * numbers_to_multiply
+        # print(result_multiplication)
+        numbers_to_multiply -= 1
+        sum_ += result_multiplication
+        # print(sum_)
+    except ValueError as e:
+        # print(e)
+        continue
+
+first_number = sum_ * 10 % 11
+if first_number > 9:
+    print(0)
+else:
+    print(first_number)
