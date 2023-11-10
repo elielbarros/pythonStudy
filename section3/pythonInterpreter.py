74aae168b0a998abdd3a229362c7f7e17104a15a
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-09 22:19:05 -0300

    include some informations about python interpreter

---
+++ section3/pythonInterpreter.py
@@ -0,0 +1,17 @@
+"""
+Python Interpreter
+
+python --help
+
+python mod.py (execute mod)
+
+python -u (unbuffered) (will execute file without save nothing on buffer)
+
+python -m mod (Will execute a library with scripts) (it is possible to use the library venv to generate a file
+environment)
+
+python -c 'command' (Will use python to execute a command, for example print('Hello world'))
+
+python -i stringJoin.py (Will enter on iterative python and could execute a file on this mode. To get out just press
+quit())
+"""
