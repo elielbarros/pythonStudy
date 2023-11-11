457d9c85fac8e20c5ed3e8cac2c0065a616bb821
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-11 13:45:20 -0300

    include second number cpf calculation

--- section3/exercise9CPFGenerator.py
+++ section3/exercise9CPFGenerator.py
@@ -5,11 +5,10 @@ CPF: 746.824.890-70
 """
 
 cpf = '746.824.890-70'
-split_cpf = cpf.split('-')
 substring_cpf = cpf[:11]
 numbers_to_multiply = 10
 sum_ = 0
-for digit in split_cpf[0]:
+for digit in substring_cpf:
     try:
         digit_int = int(digit)
         # print(digit_int)
@@ -27,3 +26,26 @@ if first_number > 9:
     print(0)
 else:
     print(first_number)
+
+substring_cpf = cpf[:13]
+numbers_to_multiply = 11
+sum_ = 0
+for digit in substring_cpf:
+    try:
+        digit_int = int(digit)
+        # print(digit_int)
+        result_multiplication = digit_int * numbers_to_multiply
+        # print(result_multiplication)
+        numbers_to_multiply -= 1
+        sum_ += result_multiplication
+        # print(sum_)
+    except ValueError as e:
+        # print(e)
+        continue
+
+second_number = sum_ * 10 % 11
+if first_number > 9:
+    print(0)
+else:
+    print(second_number)
+
