2d600e3e389e35a30e4eb13e9abb2ac4d0e82ca7
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-12 12:27:48 -0300

    include function and closure

---
+++ section4_intermediary/function_and_closure.py
@@ -0,0 +1,28 @@
+"""
+Closure And Functions that will return others functions.
+
+A closure allows a function to capture local variables from an external function, even if that external function has
+already finished executing.
+"""
+
+
+def multiplier(n):
+    def intern_function(x):
+        return x * n
+
+    return intern_function
+
+
+# Creating Functions using Closure
+double = multiplier(2)
+triple = multiplier(3)
+
+# Calling Functions
+print(double(5))  # Output: 10
+print(triple(5))  # Output: 15
+
+for number in [1, 2, 3, 4, 5]:
+    print(f'{double(number)=}')
+
+for number in [1, 2, 3, 4, 5]:
+    print(f'{triple(number)=}')
--- section4_intermediary/function_higher_order.py
+++ section4_intermediary/function_higher_order.py
@@ -4,6 +4,16 @@ Function works as variable
 It is possible to define a function and use it as a variable.
 
 It is possible to pass a defined function as an argument for another function
+
+Academically, the terms Higher Order Functions and First-Class Functions have different meanings.
+
+Higher Order Functions - Functions that can receive and/or return other functions
+
+First-Class Functions - Functions that are treated like other common data types (strings, integers, etc...)
+
+It would make little difference to your code, but I think I should tell you that.
+
+Note: These terms can be different and still reflect the same meaning.
 """
 
 
@@ -13,11 +23,11 @@ def salutation(argument):
 
 print(salutation('Hello world!'))
 
-salutation_variable = salutation
+salutation_variable = salutation  # First-Class Functions
 print(salutation_variable('Hello World, Salutation Variable!'))
 
 
-def execute_function(function_argument, salutation_argument):
+def execute_function(function_argument, salutation_argument):  # Higher Order Functions
     return function_argument(salutation_argument)
 
 
