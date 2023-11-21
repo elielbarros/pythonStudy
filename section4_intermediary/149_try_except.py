997c5176b4077c91469b11e4414393cf56c6ed71
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-20 23:11:39 -0300

    include class 147 try except

---
+++ section4_intermediary/149_try_except.py
@@ -0,0 +1,31 @@
+"""
+Try - Try to execute a code
+
+Except - Catch except raised by try block
+
+Finally - Will execute after try or except
+"""
+
+string_ = 'test'
+a = 1
+b = 0
+
+try:
+    convert = int(string_)
+    result = a / b
+except ZeroDivisionError as zeroDivisionError:
+    print("Error", zeroDivisionError)
+except ValueError as valueError:
+    print("Error", valueError)
+except Exception:
+    print("Unknown error")
+
+
+try:
+    result = a / b
+    convert = int(string_)
+except (ZeroDivisionError, ValueError) as generalError:
+    print("Error", generalError)
+except Exception:
+    print("Unknown error")
+
