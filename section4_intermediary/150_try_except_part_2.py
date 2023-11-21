387a1c2b79bf3892f547475dff7691128b0e3e39
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-21 11:58:53 -0300

    include try except part 2

---
+++ section4_intermediary/150_try_except_part_2.py
@@ -0,0 +1,18 @@
+"""
+Try - Try to execute a code
+
+Except - Catch except raised by try block
+
+Finally - Will execute after try or except
+"""
+a = 1
+b = 0
+
+try:
+    result = a / b
+except (ZeroDivisionError, ValueError) as generalError:
+    # It is possible to get the class name error
+    print('Class Error Name:', generalError.__class__.__name__)
+    print('Error:', generalError)
+except Exception:
+    print('Unknown error')
