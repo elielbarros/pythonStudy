1f38dcbd584e86942b92bf68aaee1399b1e8e074
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-22 21:51:02 -0300

    include class 162 exercise resolved

---
+++ section4_intermediary/162_exercise.py
@@ -0,0 +1,19 @@
+def sum_(x, y):
+    return x + y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def execute_function(function, x):
+    def intern_function(y):
+        return function(x, y)
+    return intern_function
+
+
+sum_with_five = execute_function(sum_, 5)
+print(sum_with_five(5))
+
+multiply_by_ten = execute_function(multiply, 10)
+print(multiply_by_ten(10))
