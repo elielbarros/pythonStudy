f0be195eab2a72e131f4de36406bdec2286446c2
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-25 09:33:58 -0300

    include decorated functions param

---
+++ section4_intermediary/167_decorated_functions_params.py
@@ -0,0 +1,41 @@
+"""
+English:
+Decorators are a powerful and flexible way to modify or extend the behavior of functions without changing their
+actual code. Decorators are functions that take another function as an argument and return a new function that
+usually extends or modifies the behavior of the original function.
+
+Portuguese:
+Decoradores são uma forma poderosa e flexível de modificar ou estender o comportamento de funções sem alterar seu
+código real. Decoradores são funções que recebem outra função como argumento e retornam uma nova função que
+geralmente estende ou modifica o comportamento da função original.
+"""
+
+
+def decorated_function_factory(a, b, c):
+    print('Decorated Function Factory')
+
+    def function_factory(function):
+        print('Function Factory')
+
+        def inner_function(*args, **kwargs):
+            print('Inner function')
+            print('Decorated Function Factory Arguments:', a, b, c)
+            res = function(*args, **kwargs)
+            return res
+
+        return inner_function
+
+    return function_factory
+
+
+@decorated_function_factory(1, 2, 3)
+def sum_(a, b):
+    return a + b
+
+
+ten_plus_five = sum_(10, 5)
+print(ten_plus_five)
+# output:
+# Decorated function
+# Inner function
+# 15
