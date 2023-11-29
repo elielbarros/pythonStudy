0a93392fcb16d11e08a79e211e781378a1c87ee7
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-28 21:00:49 -0300

    include class 176 about map method

---
+++ section4_intermediary/176_map_partial.py
@@ -0,0 +1,74 @@
+"""
+# English
+map - will map data
+partial - work as a closure function
+
+# Portuguese
+map - Serve para mapear dados
+partial - funciona como uma 'closure function'
+"""
+from functools import partial
+from types import GeneratorType
+
+products = [
+    {'name': 'Product 5', 'price': 10.00},
+    {'name': 'Product 1', 'price': 22.32},
+    {'name': 'Product 3', 'price': 10.11},
+    {'name': 'Product 2', 'price': 105.87},
+    {'name': 'Product 4', 'price': 69.90},
+]
+
+print(*products, sep='\n')
+
+print()
+
+
+def apply_percentage(value, percentage):
+    return round(value * percentage, 2)
+
+
+apply_ten_per_cent = partial(
+        apply_percentage,
+        percentage=1.1
+)
+
+new_product = [
+    {**product, 'price': apply_ten_per_cent(product['price'])}
+    for product in products
+]
+
+# Here we have new product as a list
+print(type(new_product))
+print(*new_product, sep='\n')
+
+print()
+
+
+def change_product_price(product):
+    return {**product, 'price': apply_ten_per_cent(product['price'])}
+
+
+# It is possible to map a product as list comprehension with map() method
+# map will could receive a method to apply a map and the list to map
+# It is important to understand that map as generator are exhaustible, has an end.
+# If we iterate over new product mapped, we will get on an end.
+# If we always want to have access to map values, we have to convert it using list
+# For example list(map())
+new_product_mapped = map(change_product_price, products)
+
+print(*new_product_mapped, sep='\n')
+print(new_product_mapped)
+print(hasattr(new_product_mapped, '__iter__'))
+print(hasattr(new_product_mapped, '__next__'))
+
+# Here we have a generator object, that is different of a map object
+# But has iter and next because it is iterable
+new_product_generator = (product for product in products)
+print(new_product_generator)
+print(hasattr(new_product_generator, '__iter__'))
+print(hasattr(new_product_generator, '__next__'))
+print(isinstance(new_product_generator, GeneratorType))
+
+print(
+        *map(lambda x: x * 2, [1, 2, 3, 4]), sep='\n'
+)
