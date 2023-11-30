70d1d3258281b19a7a3cecceee6878707c24c87e
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-29 21:58:56 -0300

    include class 182 how create venv

---
+++ section4_intermediary/182_how_create_venv.py
@@ -0,0 +1,10 @@
+"""
+# English
+To create a Python Environment, we run the command on terminal:
+- python -m venv environment
+
+Inside ambient, we have 'Lib' that maintains all libraries installed. We also have 'Scripts' that maintains all
+executable that will be used by our Virtual Environment
+
+To see where is our python on Linux, execute: which python
+"""
