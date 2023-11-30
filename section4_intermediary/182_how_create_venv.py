47fb6b61d60b14455cbfe63a9507437fe32d0a58
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-29 22:18:09 -0300

    include class 183 activating and deactivating venv

--- section4_intermediary/182_how_create_venv.py
+++ section4_intermediary/182_how_create_venv.py
@@ -1,10 +1,21 @@
 """
 # English
 To create a Python Environment, we run the command on terminal:
-- python -m venv environment
+python -m venv environment
 
-Inside ambient, we have 'Lib' that maintains all libraries installed. We also have 'Scripts' that maintains all
+Inside environment, we have 'Lib' that maintains all libraries installed. We also have 'Scripts' that maintains all
 executable that will be used by our Virtual Environment
 
-To see where is our python on Linux, execute: which python
+To see where is our python on Linux, execute the command on terminal:
+which python
+
+# Portuguese
+Para criar um ambiente virtual python, precisamos rodar o seguinte comando no terminal:
+python -m venv environment
+
+Dentro da pasta environment, nos temos a pasta 'Lib' que contem todas as bibliotecas instaladas. Nos tambem temos os
+'Scripts' que contem todos os executaveis usados pelo ambiente virtual.
+
+Para ver onde estah o caminho do python, no linux, execute o comando no terminal:
+which python
 """
---
+++ section4_intermediary/183_activating_venv.py
@@ -0,0 +1,27 @@
+"""
+# English
+How activate Virtual Environment
+Inside the directory 'bin' that maintain the file 'activate' execute the command:
+source activate
+
+For example:
+source /home/eliel/Documents/projects/pythonStudy/venv/bin/activate
+. /home/eliel/Documents/projects/pythonStudy/venv/bin/activate
+
+How deactivate Virtual Environment
+After activate environment, on terminal, just execute the command:
+deactivate
+
+# Portuguese
+Como ativar o Ambiente Virtual
+Dentro da pasta 'bin' onde se encontra o arquivo 'activate' execute o comando:
+source activate
+
+Por exemplo:
+source /home/eliel/Documents/projects/pythonStudy/venv/bin/activate
+. /home/eliel/Documents/projects/pythonStudy/venv/bin/activate
+
+Como desativar o Ambiente Virtual
+Apos ativar o Ambiente Virtual, no terminal, apenas execute o comando:
+deactivate
+"""
