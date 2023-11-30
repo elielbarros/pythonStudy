6a001fd26054a3928cf3a49a21d9501bfa77e59f
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-29 21:49:14 -0300

    include class 181 about virtual environment python

---
+++ section4_intermediary/181_venv.py
@@ -0,0 +1,23 @@
+"""
+# English
+Virtual Environment
+A virtual environment allows you to create a separate environment for each project, avoiding conflicts between
+libraries and versions used in different projects. This is especially useful when you work on multiple projects or
+when you need to ensure compatibility of specific library versions.
+
+The installation is inside the venv folder
+
+The external dependencies installed are also in the venv folder, however, there is a text file called
+requirements, which stores these dependencies and their respective version
+
+# Portuguese
+Ambiente Virtual
+Um ambiente virtual permite que você crie um ambiente separado para cada projeto, evitando conflitos entre as
+bibliotecas e versões utilizadas em projetos diferentes. Isso é especialmente útil quando você trabalha em múltiplos
+projetos ou quando precisa garantir a compatibilidade de versões específicas de bibliotecas.
+
+A instalação fica dentro da pasta venv
+
+As dependencias externas instaladas, tambem ficam na pasta venv, porem, existe um arquivo de texto chamado
+requirements, que guarda a informacao dessas dependencias e sua respectiva versao
+"""
