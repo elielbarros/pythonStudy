b96b5a1b24315fa0eb743a233e30f8b183c646b5
Eliel Silva <elielbarros.silva@gmail.com>
2023-11-30 21:45:57 -0300

    include class 185 about requirements.txt

---
+++ section4_intermediary/185_requirements_txt_intro.py
@@ -0,0 +1,34 @@
+"""
+# English
+
+The file requirements.txt is usually used for list and specify all necessary dependencies to the project.
+It will perform a fundamental paper on dependencies management and developing environment replication.
+
+To create requirements.txt from project, run the command at terminal:
+pip freeze > requirements.txt
+
+After that, requirements.txt will be create with all project packages included
+
+To install the packages inside requirements.txt in a recently started project we have to run the command:
+pip install -r path-to/requirements.txt
+
+The packages will be installed inside venv project, if this directory does not exist, the packages will be installed
+on global venv python directory
+
+# Portuguese
+
+O arquivo requirements.txt em Python é comumente utilizado para listar e especificar as dependências necessárias para
+um projeto. Ele desempenha um papel fundamental no gerenciamento de dependências e na replicação do ambiente de
+desenvolvimento.
+
+Para criar requirements.txt do projeto, eh necessario rodar o comando no terminal:
+pip freeze > requirements.txt
+
+Apos isso, requirements.txt serah criado com todos os packages do projeto incluso
+
+Para instalar os packages presentes em requirements.txt em um projeto recem iniciado, precisamos rodar o comando:
+pip install -r caminho-para/requirements.txt
+
+Os packages serao instalados na pasta venv do projeto, se nao existir a pasta, os packages serao instalados na venv
+global do python
+"""
