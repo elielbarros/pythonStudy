e0da02dc5ac87c0a503e7bc145bc7151e6a4bed2
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-05 22:04:26 -0300

    class 196 about positional only arguments and keyword only arguments

--- section4_intermediary/196_1_positional_only_arguments.py
+++ section4_intermediary/196_1_positional_only_arguments.py
@@ -1,22 +1,52 @@
 """
+# English
+
 PEP 570 - Python Positional-Only Parameters
 https://peps.python.org/pep-0570/
 
-Will define that the method will accept ONLY POSITIONAL PARAMETERS
+Define that the method will accept ONLY POSITIONAL PARAMETERS
 
 How does that?
 
 Using '/' as parameter
 
+NOTE: Everything coming before slash '/' cannot be NAMED ARGUMENT. Everything after slash '/' could be NAMED ARGUMENT.
+
 Look example below
+
+# Portuguese
+
+PEP 570 - Python Positional-Only Parameters
+https://peps.python.org/pep-0570/
+
+Define que o método aceitará APENAS PARÂMETROS POSICIONAIS
+
+Como é que isso?
+
+Usando '/' como parâmetro
+
+NOTA: Tudo o que vem antes da barra '/' não pode ser NOMEADO ARGUMENTO. Tudo após a barra '/' pode ser NOMEADO
+ARGUMENTO.
+
+Veja exemplo abaixo
 """
 
 
 def sum_(a, b, /):
     print(a + b)
 
+
 # it is possible to send positional parameters ONLY
 sum_(1, 2)  # output: 3
 
+
 # But if we try to pass named argument, it will raise exception:
 # sum_(a=1, b=2)  # output: TypeError: sum_() got some positional-only arguments passed as keyword arguments: 'a, b'
+
+# Another important information is that, arguments after '/' (slash) could be used as named argument
+# See the example below
+def subtraction(a, b, /, x, y):
+    return a - b - x - y
+
+
+print(subtraction(1, 2, x=3, y=4))  # output: -8
--- section4_intermediary/196_2_keyword_only_arguments.py
+++ section4_intermediary/196_2_keyword_only_arguments.py
@@ -1,5 +1,53 @@
 """
+# English
+
 PEP 3102 – Keyword-Only Arguments
 https://peps.python.org/pep-3102/
+
+Define where will end Positional Arguments. After that, we will have at least one NAMED ARGUMENT.
+
+To define it, we will use '*' to denote this limit.
+
+Before '*' everything must be Positional Argument or Named Argument.
+
+After '*' everything MUST BE Named Argument.
+
+If we want to use only Positional Argument before '*' is just use '/' too.
+
+Look example below
+
+# Portuguese
+
+PEP 3102 – Keyword-Only Arguments
+https://peps.python.org/pep-3102/
+
+Define ateh onde os Argumentos Posicionais podem ser usados. Apos isso, nos teremos, pelo menos um Argumento Nomeado.
+
+Para definir isso, nos temos que usar '*' para denotar esse limite.
+
+Antes de '*' tudo precisa ser Argumento Posicional ou Argumento Nomeado.
+
+Depois de '*' tudo PRECISA ser Argumento Nomeado.
+
+Se quisermos usar apenas o argumento posicional antes de '*', basta usar '/' também.
+
+Olhe o exemplo abaixo
+
 """
 
+
+def sum_(a, b, *, c):
+    print(a + b + c)
+
+
+# sum_(1, 2, 3)  # output: TypeError: sum_() takes 2 positional arguments but 3 were given
+sum_(1, 2, c=3)  # output: 6
+sum_(1, b=2, c=3)  # output: 6
+
+
+def sub_(a, b, /, *, c):
+    print(a - b - c)
+
+
+sub_(1, 2, c=3)  # output: -4
+# sub_(1, b=2, c=3) TypeError: subtraction_() got some positional-only arguments passed as keyword arguments: 'b'
