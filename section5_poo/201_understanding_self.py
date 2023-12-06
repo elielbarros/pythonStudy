de1ef7a96834b75eea53909d3a2b79ef365207cc
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-05 23:26:54 -0300

    include class 201 about 'self'

---
+++ section5_poo/201_understanding_self.py
@@ -0,0 +1,17 @@
+"""
+# English
+
+'self' is a convention to make a reference to class.
+
+to define any method inside a class, we must use self.
+
+'self' will represent the instance created by the class.
+
+# Portuguese
+
+'self' eh uma convensao que faz referencia a classe.
+
+para definir qualquer metodo que seja, na classe, precisamos usar 'self'
+
+'self' vai representar a instancia criada em relacao a classe.
+"""
