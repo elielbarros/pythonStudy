bee3871c5fd0a2292fc593d553cece01dcd68714
Eliel Barros <58311651+elielbarros@users.noreply.github.com>
2023-12-08 11:09:28 -0300

    class 207 about exercise

--- section5_oop/exercise/206_exercise_read_json.py
+++ section5_oop/exercise/206_exercise_read_json.py
@@ -1,5 +1,18 @@
 import json
 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+person_list = []
 with open('206_json.json', 'r') as file:
-    person = json.load(file)
-    print(person)
+    person_list_dict = json.load(file)
+
+    for person_dict in person_list_dict:
+        person_list.append(Person(**person_dict))
+
+for person in person_list:
+    print(person.name, person.age)
--- section5_oop/exercise/206_exercise_write_json.py
+++ section5_oop/exercise/206_exercise_write_json.py
@@ -1,9 +1,17 @@
 import json
 
-person = {
-    'name': 'John',
-    'age': 28,
-}
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 
 with open('206_json.json', 'w') as file:
-    json.dump(person, file, ensure_ascii=False, indent=2)
+    person = Person('John', 28)
+    person1 = Person('Mary', 24)
+    person_list = [person.__dict__, vars(person1)]
+    # If we try to make dump of person as class it will raise an exception
+    # json.dump(person, file, ensure_ascii=False, indent=2) # TypeError: Object of type Person is not JSON serializable
+    # To resolve that, we will convert Person to a dict using vars() or __dict__
+    json.dump(person_list, file, ensure_ascii=False, indent=2)
