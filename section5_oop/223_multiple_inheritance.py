f6ee7259a3424bfdcf334cffaf6e59f4de0b15a0
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-13 22:17:59 -0300

    include class 223 about multiple iniheritance

---
+++ section5_oop/223_multiple_inheritance.py
@@ -0,0 +1,19 @@
+"""
+# English
+- Multiple Inheritance
+- Multiple Inheritance is the capacity of the Class to extend from another class.
+
+ClassA(ClassB, ClassC)
+
+To know about the call order about the methods, it is necessary to use the class method Class.mro() or the attribute
+__mro__
+
+# Portuguese
+- Heranca Multipla
+- Heranca Multipla eh a capacidade de uma classe estender de outras classes.
+
+ClassA(ClassB, ClassC)
+
+Para saber ordem de chamada dos metodos usar o metodo de classe Classe.mro() ou o atributo __mro__ (Dunder - Double
+Underscore)
+"""
