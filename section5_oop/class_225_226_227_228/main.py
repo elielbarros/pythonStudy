8b8a28facd51b27f161d678b178049ec32c43898
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-15 22:53:27 -0300

    include class 228

--- section5_oop/class_225_226_227_228/main.py
+++ section5_oop/class_225_226_227_228/main.py
@@ -1,4 +1,11 @@
 from log import LogFileMixin
+from eletronic import Smartphone
 
 log_file = LogFileMixin()
 log_file.log_success('Raising Success')
+
+galaxy = Smartphone('Galaxy S')
+iphone = Smartphone('iPhone')
+
+galaxy.turn_on()
+iphone.turn_off()
