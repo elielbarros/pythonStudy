b6988220a278361830e46e554637d9c836646032
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-16 23:02:59 -0300

    include class 231 about polymorphism theory

---
+++ section5_oop/231_polymorphism_theory.py
@@ -0,0 +1,23 @@
+"""
+# English
+Polymorphism - Python Object Oriented
+
+Polymorphism is a principle that allows derivative classes from super classes to have equal methods (with same
+signature) but different implementation.
+
+Method return do not make part of this method signature.
+
+Liskov Substitution Principle - SO "L" ID
+Objects from superclass must be replaceable by subclass object without breaking.
+
+# Portuguese
+Polimorfismo - Python Orientado a Objetos
+
+Polimorfismo eh um principio que permite que classes derivadas de uma mesma superclasse tenham metodos iguais (com
+mesma assinatura) mas implementacao diferente.
+
+O retorno do metodo nao faz parte da assinatura do metodo.
+
+Principio da substituicao de liskov - SO "L" ID
+Objetos de uma superclasse devem ser substituíveis por objetos de uma subclasse sem quebrar a aplicacao.
+"""
