9d949427c9c0c586db19a45e51a0e62be1315be6
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-18 19:40:58 -0300

    include class 236 dunder methods theory

---
+++ section5_oop/236_dunder_methods_theory.py
@@ -0,0 +1,36 @@
+"""
+# English
+Dunder means DOUBLE UNDERSCORE = __dunder__
+
+Dunder makes a reference to Special Methods or Magic Methods
+
+https://rszalski.github.io/magicmethods/
+https://docs.python.org/3/reference/datamodel.html#specialnames
+
+Comparison magic methods
+__lt__(self,other) - self < other
+__le__(self,other) - self <= other
+__gt__(self,other) - self > other
+__ge__(self,other) - self >= other
+__eq__(self,other) - self == other
+__ne__(self,other) - self != other
+
+Normal arithmetic operators
+__add__(self,other) - self + other
+__sub__(self,other) - self - other
+__mul__(self,other) - self * other
+__truediv__(self,other) - self / other
+
+Unary operators and functions
+__neg__(self) - -self
+
+Representing class
+__str__(self) - str  Defines behavior for when str() is called on an instance of your class.
+__repr__(self) - str repr() is intended to produce output that is mostly machine-readable (in many cases, it could be
+valid Python code even), whereas str() is intended to be human-readable.
+
+# Portuguese
+Dunder significa duplo underscore = __dunder__
+
+Dunder faz referencia a Metodos Especiais ou Metodos Magicos
+"""
