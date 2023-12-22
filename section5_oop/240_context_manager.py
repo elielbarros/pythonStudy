7adc981e41c3c0acf3949e5ec0058d0848238d24
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-21 21:37:22 -0300

    include class 240 about context manager

--- section5_oop/240_context_manager.py
+++ section5_oop/240_context_manager.py
@@ -1,41 +1,52 @@
-# Context Manager com classes - Criando e Usando gerenciadores de contexto
-# Você pode implementar seus próprios protocolos
-# apenas implementando os dunder methods que o
-# Python vai usar.
-# Isso é chamado de Duck typing. Um conceito
-# relacionado com tipagem dinâmica onde o Python não
-# está interessado no tipo, mas se alguns métodos existem
-# no seu objeto para que ele funcione de forma adequada.
-# Duck Typing:
-# Quando vejo um pássaro que caminha como um pato, nada como
-# um pato e grasna como um pato, eu chamo aquele pássaro de pato.
-# Para criar um context manager, os métodos __enter__ e __exit__
-# devem ser implementados.
-# O método __exit__ receberá a classe de exceção, a exceção e o
-# traceback. Se ele retornar True, exceção no with será
-# suprimidas.
-#
-# Ex:
-# with open('aula149.txt', 'w') as arquivo:
-#     ...
+"""
+# English
+Context Manager with classes - Creating and Using context managers.
+You can implement your own protocols just by implementing the dunder methods that Python will use.
+This is called Duck typing, a concept related to dynamic typing where Python is not interested in
+type, but if some methods exist in your object for it to work properly.
+Duck Typing:
+When I see a bird that walks like a duck, swims like a duck, and quacks like a duck, I call that bird a
+duck.
+To create a context manager, the __enter__ and __exit__ methods must be implemented.
+The __exit__ method will receive the exception class, exception and traceback. If it returns True, exception in with
+will be deleted.
+Ex:
+with open('aula149.txt', 'w') as arquivo:
+
+# Portuguese
+Context Manager com classes - Criando e Usando gerenciadores de contexto
+Você pode implementar seus próprios protocolos apenas implementando os dunder methods que o Python vai usar.
+Isso é chamado de Duck typing. Um conceito relacionado com tipagem dinâmica onde o Python não está interessado no
+tipo, mas se alguns métodos existem no seu objeto para que ele funcione de forma adequada.
+Duck Typing:
+Quando vejo um pássaro que caminha como um pato, nada como um pato e grasna como um pato, eu chamo aquele pássaro de
+pato.
+Para criar um context manager, os métodos __enter__ e __exit__ devem ser implementados.
+O método __exit__ receberá a classe de exceção, a exceção e o traceback. Se ele retornar True, exceção no with será
+suprimidas.
+Ex:
+with open('aula149.txt', 'w') as arquivo:
+"""
+
+
 class MyOpen:
-    def __init__(self, caminho_arquivo, modo):
-        self.caminho_arquivo = caminho_arquivo
-        self.modo = modo
-        self._arquivo = None
+    def __init__(self, file_path, mode):
+        self.file_path = file_path
+        self.mode = mode
+        self._file = None
 
     def __enter__(self):
-        print('ABRINDO ARQUIVO')
-        self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8')
-        return self._arquivo
+        print('Open File')
+        self._file = open(self.file_path, self.mode, encoding='utf8')
+        return self._file
 
     def __exit__(self, class_exception, exception_, traceback_):
-        print('FECHANDO ARQUIVO')
-        self._arquivo.close()
+        print('Close File')
+        self._file.close()
 
 
-with MyOpen('aula149.txt', 'w') as arquivo:
-    arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n')
-    arquivo.write('Linha 3\n')
-    print('WITH', arquivo)
+with MyOpen('240_class.txt', 'w') as file:
+    file.write('Line 1\n')
+    file.write('Line 2\n')
+    file.write('Line 3\n')
+    print('WITH', file)
