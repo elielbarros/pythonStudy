aff244f8c83d117746d1c12bf8755f7a76c54d21
Eliel Silva <elielbarros.silva@gmail.com>
2023-12-26 21:41:34 -0300

    include class 254 about the theory of enum

---
+++ section5_oop/254_enum_theory.py
@@ -0,0 +1,27 @@
+"""
+# English
+Enum - Enumeration
+
+Collection of named values, representing constants that should not be changed during program execution. Enums provide
+a way to associate human-readable labels with integer values or strings, providing code clarity and readability.
+
+Enum in Python is a class with a different Metaclass.
+
+To get Enum data in Python
+key = Class.key.name
+value = Class.key.value
+
+# Portuguese
+Enum - Enumeracoes
+
+Coleção de valores nomeados, representando constantes que não devem ser alteradas durante a execução do programa.
+Enums oferecem uma maneira de associar rótulos legíveis a valores inteiros ou strings, proporcionando clareza e
+legibilidade ao código.
+
+Enum em Python eh uma classe com uma metaclasse diferente.
+
+Para obter dados de um Enum em Python
+chave = Classe.chave.name
+valor = Classe.chave.value
+"""
+
