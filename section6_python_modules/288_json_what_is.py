7ffff41b78a2f682b6bf378c8f77fe75599b66f2
Eliel Silva <elielbarros.silva@gmail.com>
2024-01-13 23:34:15 -0300

    include class 288 about json

---
+++ section6_python_modules/288_json_what_is.py
@@ -0,0 +1,58 @@
+"""
+# English
+What is JSON?
+
+JSON - JavaScript Object Notation (extension .json)
+
+JSON is a common data format used to represent simple data structures and objects, mainly in communication contexts
+between web systems.
+
+JSON is widely used in communication between servers and clients on the web, in API (Application Programming
+Interface) configurations and in many other contexts where simplicity, readability and interoperability are important.
+
+JSON supports:
+- Number: int, float
+- String: "Hello World" NOTE: You must use double quotes
+- Boolean: true or false
+- Lists: [1,2,3] ["Hello", "World", "!"]
+- Objects: {"name":"John", "age":100}
+- null
+
+PYTHON -> JSON
+dict          object
+list, tuple   array
+str           string
+int, float    number
+True          true
+False         false
+None          null
+
+# Portuguese
+O que eh JSON?
+
+JSON - JavaScript Object Notation (extensao .json)
+
+JSON é um formato de dados comum usado para representar estruturas de dados simples e objetos, principalmente em
+contextos de comunicação entre sistemas web.
+
+JSON é amplamente utilizado na comunicação entre servidores e clientes na web, em configurações de APIs (Interface de
+Programação de Aplicações) e em muitos outros contextos onde a simplicidade, legibilidade e interoperabilidade são
+importantes.
+
+JSON suporta:
+- Numero: int, float
+- String: "Hello World" OBS.: Deve usar aspas duplas
+- Boolean: true or false
+- Listas: [1,2,3] ["Hello", "World", "!"]
+- Objetos: {"nome":"John", "idade":100}
+- null
+
+PYTHON -> JSON
+dict          object
+list, tuple   array
+str           string
+int, float    number
+True          true
+False         false
+None          null
+"""
