c9ee4148142c5ac7e6c5f2395ddd77abf87e5dcd
Eliel Silva <elielbarros.silva@gmail.com>
2024-01-16 22:23:31 -0300

    include class 292 introduction to csv

---
+++ section6_python_modules/292_csv_comma_separated_values.py
@@ -0,0 +1,43 @@
+"""
+# English
+CSV (Comma Separated Values)
+
+Extension - .csv
+
+Could be opened using text editor or Electronic spreadsheet (Google Sheets, Excel, LibreOffice Calc)
+
+Example:
+    name,age,address
+    john, 500,'Avenue Something'
+
+The First line defines column names.
+
+Next lines contain values.
+
+Simple Rules:
+ 1. Split column values with a unique delimiter (,)
+ 2. Every register must be in one raw
+ 3. Do not leave lines or spaces left
+ 4. Use scape character (") when delimiter appears on value
+
+# Portuguese
+CSV (Valores separados por virgula)
+
+Extensao - .csv
+
+Pode ser aberto usando editor de texto ou planilha eletronica (Google Sheets, Excel, LibreOffice Calc)
+
+Exemplo:
+    nome,idade,endereco
+    john,500,'avenida qualquer'
+
+A primeira linha define os nomes das colunas.
+
+Proximas linhas contem valores.
+
+Regras simples:
+ 1. Separe os valoroes das colunas com um delimitador unico (,)
+ 2. Cada regiistro deve estar em uma linha
+ 3. Nao deixar linhas ou espaços sobrando
+ 4. Use o caracter de escape (") quando o delimitador aparecer no valor
+"""
