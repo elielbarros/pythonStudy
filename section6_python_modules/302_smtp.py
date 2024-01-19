b5f2b13a82c87fd639bb9e39852ed0fd77cfaa67
Eliel Silva <elielbarros.silva@gmail.com>
2024-01-19 20:32:15 -0300

    include class 302 sending email using python

---
+++ section6_python_modules/302_smtp.py
@@ -0,0 +1,17 @@
+"""
+# English
+1. Access Google Email Settings
+2. Access the "Forwarding and POP/IMAP" tab
+3. Activate IMAP
+4. Access Account Settings
+5. Search by App Password
+6. Generate an App Password
+
+# Portuguese
+1. Acessar Configuracoes do e-mail google
+2. Acessar a aba "Encaminhamento e POP/IMAP"
+3. Ativar IMAP
+4. Acessar Configuracoes de Conta
+5. Pesquisar por Senha de App
+6. Gerar uma Senha de App
+"""
