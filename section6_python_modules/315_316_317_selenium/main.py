2ecfed705bf813d18b887c90c0430a6cd3848d5b
Eliel Barros <58311651+elielbarros@users.noreply.github.com>
2024-01-27 22:51:46 -0300

    include class 318 about selenium

--- section6_python_modules/315_316_317_selenium/main.py
+++ section6_python_modules/315_316_317_selenium/main.py
@@ -5,6 +5,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -57,7 +58,7 @@ if __name__ == '__main__':
 
     try:
         search_input.send_keys('Hello World')
-        search_input.submit()
+        search_input.send_keys('Keys.ENTER')
     except Exception as e:
         print(e)
 
