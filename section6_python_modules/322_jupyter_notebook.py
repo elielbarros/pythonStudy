cf50fc7020b08ed517b49321c8aa559b0fc42dc9
Eliel Silva <elielbarros.silva@gmail.com>
2024-01-29 22:29:15 -0300

    include class 322 about jupyter notebook

---
+++ section6_python_modules/322_jupyter_notebook.py
@@ -0,0 +1,43 @@
+"""
+# English
+Jupyter Notebook
+
+- How Install
+    pip install notebook
+
+- How run
+    jupyter notebook
+
+1. In 'Files' click the button 'New' and select 'Notebook'
+2. Select for Python 3 (ipykernel)
+3. Click on text 'Untitled' and change the title
+4. Select for 'Markdown' to write some text
+5. Select for 'Code' to write some code
+6. Execute code with Button Double Play
+
+# Portuguese
+Jupyter Notebook
+
+- Como instalar
+    pip install notebook
+
+- Como rodar
+    jupyter notebook
+
+1. Em 'Files' clique no botão 'New' e selecione 'Notebook'
+2. Selecione 'Python 3 (ipykernel)'
+3. Clique no texto 'Untitled' e altere o titulo
+4. Selecione 'Markdown' para escrever algum texto
+5. Selecione 'Code' para escrever algum codigo
+6. Execute o codigo clicando no botao 'Double Play'
+
+"""
+
+# This line maintain the value of `a`
+a = 2
+
+# This line maintain the value of `b`
+b = a * 2
+
+# This line show the value of `a` + `b`
+print(a + b)
