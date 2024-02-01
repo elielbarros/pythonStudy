7e4f9055ccbcdbc376b06af2d9b789676e467d3a
Eliel Silva <elielbarros.silva@gmail.com>
2024-01-31 23:01:12 -0300

    include class 327 about PyPDF2

---
+++ section6_python_modules/PyPDF2/327_PyPDF2.py
@@ -0,0 +1,21 @@
+"""
+# English
+PyPDF2 to manipulate PDF files (Installation)
+PyPDF2 is a PDF file manipulation library made in pure Python, free and open source. It is capable of reading, manipulating, writing and joining data from PDF files, as well as adding annotations, transforming pages, extracting text and images, manipulating metadata, and more.
+
+The documentation contains all the information needed to use PyPDF2.
+Link: https://pypdf2.readthedocs.io/en/3.0.0/
+
+How to install?
+     pip install pypdf2
+
+# Portuguese
+PyPDF2 para manipular arquivos PDF (Instalação)
+PyPDF2 é uma biblioteca de manipulação de arquivos PDF feita em Python puro, gratuita e de código aberto. Ela é capaz de ler, manipular, escrever e unir dados de arquivos PDF, assim como adicionar anotações, transformar páginas, extrair texto e imagens, manipular metadados, e mais.
+
+A documentação contém todas as informações necessárias para usar PyPDF2.
+Link: https://pypdf2.readthedocs.io/en/3.0.0/
+
+Como instalar?
+    pip install pypdf2
+"""
