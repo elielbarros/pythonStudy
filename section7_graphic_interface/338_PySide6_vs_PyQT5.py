e312975af2951d6b65b0ebf75454e7c8cc1c11f8
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-04 16:14:16 -0300

    include class 338 about pySide6 and PyQT5

---
+++ section7_graphic_interface/338_PySide6_vs_PyQT5.py
@@ -0,0 +1,56 @@
+"""
+# English
+PySide6 for GUI (graphical interface) with Qt in Python - Installation
+- The original section of this course used PyQt5 (we are upgrading to PySide6)
+- These libraries (PySide and PyQt) use the Qt library
+- Qt is a library used for creating GUI (graphical interface
+   username) written in C++.
+   - PySide and PyQt can bridge the gap between Python and
+   library for creating graphical interfaces without having to use another
+   programming language.
+- PySide6 is a reference to version 6 of Qt (Qt 6)
+- Qt is multiplatform, that is, it must work on Windows, Linux and Mac.
+
+Why did I switch from PyQt to PySide in the upgrade?
+- PySide was developed by The Qt Company (owned by Nokia), as part of
+   Qt for Python project - https://doc.qt.io/qtforpython/
+- Because they use the same library (Qt), PySide and PyQt are extremely
+   similar, the codes are often identical. Therefore, even if you
+   Even if you want to use PyQt, it will be very simple to port the codes. Often
+   just change the name from PySide to PyQt and vice versa.
+- The biggest difference between PyQt and PySide is in the license:
+   PyQt uses GPL or commercial and PySide uses LGPL.
+   In summary: with PySide, you are allowed to use the library for
+   commercials, without having to share the code written by you for the
+   public.
+   Licenses are complex topics, so educate yourself on them
+   before using any third party software.
+
+# Portuguese
+PySide6 para GUI (interface gráfica) com Qt em Python - Instalação
+- A seção original desse curso usou PyQt5 (estamos atualizando para PySide6)
+- Essas bibliotecas (PySide e PyQt) usam a biblioteca Qt
+- Qt é uma biblioteca usada para a criação de GUI (interface gráfica
+  do usuário) escrita em C++.
+  - PySide e PyQt conseguem fazer a ponte (binding) entre o Python e a
+  biblioteca para a criação de interfaces gráficas sem ter que usar outra
+  linguagem de programação.
+- PySide6 é uma referencia à versão 6 da Qt (Qt 6)
+- Qt é multiplataforma, ou seja, deve funcionar em Windows, Linux e Mac.
+
+Por que mudei de PyQt para PySide na atualização?
+- PySide foi desenvolvido pela The Qt Company (da Nokia), como parte do
+  projeto Qt for Python project - https://doc.qt.io/qtforpython/
+- Por usarem a mesma biblioteca (Qt), PySide e PyQt são extremamente
+  similares, muitas vezes os códigos são idênticos. Portanto, mesmo que você
+  ainda queira usar PyQt, será muito simples portar os códigos. Muitas vezes
+  basta trocar o nome de PySide para PyQt e vice-versa.
+- A maior diferença entre PyQt e PySide está na licença:
+  PyQt usa GPL ou commercial e PySide usa LGPL.
+  Em resumo: com PySide, você tem a permissão uso da biblioteca para fins
+  comerciais, sem ter que compartilhar o código escrito por você para o
+  público.
+  Licenças são tópicos complexos, portanto, se oriente sobre elas
+  antes de usar qualquer software de terceiros.
+  https://tldrlegal.com/license/gnu-lesser-general-public-license-v3-(lgpl-3)
+  """
