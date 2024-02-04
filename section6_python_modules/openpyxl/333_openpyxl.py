fc4bf61cc5631bcbb228f50271d0e0c85b52ea06
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-03 21:22:39 -0300

    include class 333 about openpyxl

---
+++ section6_python_modules/openpyxl/333_openpyxl.py
@@ -0,0 +1,21 @@
+"""
+# English
+Python library that allows you to work with Excel files (.xlsx). This library provides an interface for creating,
+reading, modifying, and saving Excel files in the Office Open XML (OOXML) format.
+
+How to use openpyxl?
+     0. Documentation
+         - https://openpyxl.readthedocs.io/en/stable/
+     1. Install
+         - pip install openpyxl
+
+# Portuguese
+Biblioteca em Python que permite trabalhar com arquivos Excel (.xlsx). Essa biblioteca fornece uma interface para
+criar, ler, modificar e salvar arquivos Excel no formato Office Open XML (OOXML).
+
+Como usar openpyxl?
+    0. Documentacao
+        - https://openpyxl.readthedocs.io/en/stable/
+    1. Instalar
+        - pip install openpyxl
+"""
