d47ad1f17f653fed8b05eea4e8e5a10d961172b5
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-18 18:14:01 -0300

    include class 363

--- section7_graphic_interface/calculator/buttons.py
+++ section7_graphic_interface/calculator/buttons.py
@@ -115,6 +115,9 @@ class ButtonsGrid(QGridLayout):
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
 
+        if text == '◀':
+            self._connectButtonClicked(button, self.display.backspace)
+
         if text in '+-/*^':
             self._connectButtonClicked(
                     button,
