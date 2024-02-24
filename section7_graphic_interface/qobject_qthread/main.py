5e66f74b30958792ccc33b240214ff54512353b7
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-24 16:53:28 -0300

    include class 383

--- section7_graphic_interface/qobject_qthread/main.py
+++ section7_graphic_interface/qobject_qthread/main.py
@@ -1,9 +1,22 @@
 import sys
 import time
 
+from PySide6.QtCore import QObject, Signal, Slot
 from PySide6.QtWidgets import QApplication, QWidget
 from ui_workerui import Ui_myWidget
 
+class Worker1(QObject):
+    started = Signal(str)
+    progress = Signal(str)
+    finished = Signal(str)
+
+    def run(self):
+        self.started.emit('0 %')
+        for i in range(5):
+            percentage = i*100/5
+            self.progress.emit(f'{str(percentage)} %')
+            time.sleep(1)
+        self.finished.emit('100 %')
 
 class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent=None):
@@ -14,9 +27,6 @@ class MyWidget(QWidget, Ui_myWidget):
         self.button2.clicked.connect(self.hardWork2)
 
     def hardWork1(self):
-        for i in range(5):
-            print(i)
-            time.sleep(1)
         self.label1.setText('1 ended')
 
     def hardWork2(self):
