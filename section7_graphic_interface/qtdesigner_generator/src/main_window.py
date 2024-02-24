e336f61dc2322b90b4d15bd95e58305d02113181
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-23 21:48:57 -0300

    include class 380

--- section7_graphic_interface/qtdesigner_generator/src/main_window.py
+++ section7_graphic_interface/qtdesigner_generator/src/main_window.py
@@ -1,4 +1,7 @@
 from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import QObject, QEvent
+from PySide6.QtGui import QKeyEvent
+from typing import cast
 from ui_window import Ui_MainWindow
 import sys
 
@@ -9,8 +12,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.button_send.clicked.connect(self._changeLabelResult)
 
+        self.line_name.installEventFilter(self)
+
     def _changeLabelResult(self):
         self.label_result.setText(self.line_name.text())
+
+    def eventFilter(self, watched: QObject, event: QEvent):
+
+        if event.type() == QEvent.Type.KeyPress:
+            event = cast(QKeyEvent, event)
+            print(event.text())
+            self.label_result.setText(self.line_name.text() + event.text())
+
+        return super().eventFilter(watched, event)
 
 
 if __name__ == '__main__':
