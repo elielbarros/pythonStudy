3e21d157ea3dc4d574d9d8d98cf43a7fe4811134
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-23 22:13:16 -0300

    include class 382

--- section7_graphic_interface/qobject_qthread/main.py
+++ section7_graphic_interface/qobject_qthread/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from PySide6.QtWidgets import QApplication, QWidget
 from ui_workerui import Ui_myWidget
@@ -8,6 +9,21 @@ class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.button1.clicked.connect(self.hardWork1)
+        self.button2.clicked.connect(self.hardWork2)
+
+    def hardWork1(self):
+        for i in range(5):
+            print(i)
+            time.sleep(1)
+        self.label1.setText('1 ended')
+
+    def hardWork2(self):
+        for i in range(5):
+            print(i)
+            time.sleep(1)
+        self.label2.setText('2 ended')
 
 
 if __name__ == '__main__':
