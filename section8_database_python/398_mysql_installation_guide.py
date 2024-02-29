c967d9419c04a03460bcfc6c04c27b1778c0818c
Eliel Silva <elielbarros.silva@gmail.com>
2024-02-29 12:49:55 -0300

    include class 398

---
+++ section8_database_python/398_mysql_installation_guide.py
@@ -0,0 +1,9 @@
+# Access https://www.mysql.com/downloads/
+
+# Search for MySQL Community Downloads link and click on it
+
+# You will be redirected for another page
+
+# In windows look for MySQL installer for Windows and install "Server Only"
+
+# In linux look for "MySQL Community Server" and install last version available.
